fe8fb766eb28f1633421eefae7e338f341715f99
Жаринов Георгий <Georgiy.Zharinov@omnichannel.ru>
2024-10-30 16:25:01 +0300

    .

--- Program.cs
+++ Program.cs
@@ -202,6 +202,8 @@ namespace Encryption
             Console.WriteLine($"Encrypt");
             byte[] st = KeyAdd(RoundKey[0], Text);
             Console.WriteLine("Добавление раундового ключа");
+            Console.WriteLine($"state: {Text}");
+            Console.WriteLine($"rkey: {RoundKey[0]}");
             printByteArrayInHEx(st);
             Console.WriteLine();
             for (int i = 1; i <= 7; i++)
